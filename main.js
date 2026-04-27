9125d7c37a1a8e6404398af33aec383ca57bf744
Youssef <youssef200512270@gmail.com>
2026-04-27 17:02:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ i_links.onclick = function() {
         i_links.classList.replace("fa-xmark", "fa-bars");
     }
 }
+let section = document.getElementsByClassName("section");
+let card = document.getElementsByClassName("card");
+function animation(x){
+    for(let i = 0; i < x.length; i++){
+        x[i].style.animation='fadeInUp 2s ease forwards'
+    }
+}
+window.onscroll = function() {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+        animation(section);
+    }
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        animation(card);
+    }
+};
+
+
+
+
+
+
+
+
+let Search = document.getElementById("Search");
+function focusSearch(){
+    Search.focus();
+}
+function SearchElement(value) {
+    for(let i = 0; i < value.length; i++)
+    {
+
+    }
+}
